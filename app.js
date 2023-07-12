0a316eadf1255178b4d127fc3b617372921534e1
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-12 09:15:19 -0500

    feat: :sparkles: EndPoint Usuario
    Se creo el endpoint usuario que permita traer todos los datos de la tabla ordenados alfabeticamente

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import storageUsuario from './routers/usuario.js';
-
+/* import storageCita from './routers/cita.js';
+import storageMedico from './routers/medico.js'; */
+// import storageInventarios from './routers/inventarios.js';
 dotenv.config();
 const appExpress = express();//levanta todo el sevicio
 
 appExpress.use(express.json());
 appExpress.use("/usuario", storageUsuario);
-
+/* appExpress.use("/cita", storageCita);
+appExpress.use("/medico", storageMedico); */
 
 
 const config = JSON.parse(process.env.MY_CONFIG);

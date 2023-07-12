c00e4903b9e30c551a99652be1491100980b659b
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-12 09:22:23 -0500

    feat: :sparkles: EndPoint cita
    Se creo el endpoint con la consulta de que trae los datos ordenados por fecha

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import storageUsuario from './routers/usuario.js';
-/* import storageCita from './routers/cita.js';
+import storageCita from './routers/cita.js';
+/* 
 import storageMedico from './routers/medico.js'; */
 // import storageInventarios from './routers/inventarios.js';
 dotenv.config();
@@ -9,7 +10,8 @@ const appExpress = express();//levanta todo el sevicio
 
 appExpress.use(express.json());
 appExpress.use("/usuario", storageUsuario);
-/* appExpress.use("/cita", storageCita);
+appExpress.use("/cita", storageCita);
+/* 
 appExpress.use("/medico", storageMedico); */
 
 
---
+++ routers/cita.js
@@ -0,0 +1,24 @@
+import mysql from "mysql2";
+import { Router } from "express";
+const storageCita = Router();
+
+let con = undefined;
+
+storageCita.use((req, res, next) => {
+  let myConfig = JSON.parse(process.env.MY_CONNECT);
+  con = mysql.createPool(myConfig);
+  next();
+});
+
+storageCita.get("/", (req, res) => {
+  con.query(
+    `SELECT * FROM cita ORDER BY (cit_fecha) ASC;`,
+
+    (err, data, fil) => {
+      res.send(JSON.stringify(data));
+    }
+  );
+});
+
+export default storageCita;
+

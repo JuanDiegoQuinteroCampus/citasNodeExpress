f9a1c0c1c4229ff3a28a78e2ae4cd8a866a552b4
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-14 06:43:44 -0500

    feat: :fire: Eliminación lineas comentadas, usuario
    Se eliminaron las lineas comentadas en el archivo usuario

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import storageUsuario from './routers/usuario.js';
-import storageCita from './routers/cita.js';
-/* 
-import storageMedico from './routers/medico.js'; */
-// import storageInventarios from './routers/inventarios.js';
+
+
 dotenv.config();
 const appExpress = express();//levanta todo el sevicio
 
 appExpress.use(express.json());
 appExpress.use("/usuario", storageUsuario);
 appExpress.use("/cita", storageCita);
-/* 
-appExpress.use("/medico", storageMedico); */
+appExpress.use("/medico", storageMedico);
+appExpress.use("/pacientes", storageProxCita);
+appExpress.use("/citaMedico", storageCitaMedico);
+appExpress.use("/consultorias", storageConsultorias);
+appExpress.use("/buscar", storageEncontrarCita);
+appExpress.use("/obtener", storageMedConsultorio);
+appExpress.use("/contar", storageContadorCitas);
+appExpress.use("/aplicar", storageConsultorioCitas);
+appExpress.use("/genero", storageCitasGenero);
+/* appExpress.use("/genero", storageCitasGenero); */
+appExpress.use("/rechazadas", storageDatosCita);
 
 
 const config = JSON.parse(process.env.MY_CONFIG);
--- routers/usuario.js
+++ routers/usuario.js
@@ -19,53 +19,6 @@ storageUsuario.get("/", (req, res) => {
     }
   );
 });
-// storageUsuario.post("/", (req, res) => {
-//   const {
-//     id,
-//     nombre,
-//     id_responsable,
-//     estado,
-//     created_by,
-//     update_by,
-//     created_at,
-//   } = req.body;
-
-//   const sql = `
-//       INSERT INTO bodegas (id, nombre, id_responsable, estado, created_by, update_by, created_at)
-//       VALUES (?, ?, ?, ?, ?, ?, ?)
-//     `;
-//   /* datos a ingresar
-//     {
-//   "id": 51,
-//   "nombre": "A Bodegas",
-//   "id_responsable": 11,
-//   "estado": 1,
-//   "created_by": null,
-//   "update_by": null,
-//   "created_at": "2023-05-25 01:02:57"
-// }
-//  */
-//   const values = [
-//     id,
-//     nombre,
-//     id_responsable,
-//     estado,
-//     created_by,
-//     update_by,
-//     created_at,
-//   ];
-
-//   con.query(sql, values, (err, result) => {
-//     if (err) {
-//       console.error("Error al insertar la bodega:", err);
-//       return res.status(500).json({ error: "Error interno del servidor." });
-//     }
-
-//     res.status(201).json({ mensaje: "Bodega insertada con éxito." });
-//   });
-// });
 
 export default storageUsuario;
 
-/* `INSERT INTO bodegas (id, nombre, id_responsable,estado,created_by,update_by,created_at)
-        VALUES (51, 'A Bodegas', 11, 1, NULL, NULL, '2023-05-25 01:02:57', '2023-06-07 01:02:57', NULL);`, */

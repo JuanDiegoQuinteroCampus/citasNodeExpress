b4d32e4d52ffaf5c20a15b025bbb730c3f40db73
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-17 18:41:57 -0500

    feat: :bug: Corrección a la solucion del problema
    Se hizo un cambio porque lo que estaba arrojando era los datos de la cita, y por lo tal debia mostrar unos datos del consultorio y la fecha que tenia la cita; corrección hecha

--- routers/consultorias.js
+++ routers/consultorias.js
@@ -11,21 +11,20 @@ storageConsultorias.use((req, res, next) => {
 });
 
 storageConsultorias.get('/:usu_id/pacientes', (req, res) => {
-  const usu_id = req.params.usu_id;
+
   
   con.query(
-    `SELECT c.*
-    FROM cita c
-    INNER JOIN usuario u ON c.cit_datosUsuario = u.usu_id
-    WHERE u.usu_id = ?;`,
-    [usu_id],
+    `SELECT consultorio.cons_codigo as consultorioCodigo, consultorio.cons_nombre as consultorioNombre, usuario.usu_id as pacienteCodigo, usuario.usu_nombre as pacienteNombre, usuario.usu_primer_apellido_usuar as pacienteApellido1, cita.cit_fecha as citaFecha FROM usuario INNER JOIN cita ON cita.cit_datosUsuario = usuario.usu_id INNER JOIN medico ON medico.med_nroMatriculaProsional = cita.cit_medico INNER JOIN consultorio ON consultorio.cons_codigo = medico.med_consultorio WHERE usuario.usu_id = ${req.params.usu_id};`,
+
     (err, results) => {
       if (err) {
+
         res.status(500).json({ error: 'Error en la base de datos' });
+
       } else if (results.length === 0) {
-        res.status(404).json({ message: 'No se encontraron consultorías' });
+        res.status(404).json({ message: 'No se encontró ninguna cita' });
       } else {
-        res.json({ consultorias: results });
+        res.json({ cita: results[0] });
       }
     }
   );
